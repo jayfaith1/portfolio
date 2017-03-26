330f24695a9366962d6b94f8e1a32c5e9254b0d7
James Anderson <jamesanderson@Jamess-MacBook-Pro.local>
2017-03-26 15:16:28 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    slideWidth: 350,
+    speed: 500,
+    minSlides: 2,
+    maxSlides: 3,
+    auto: true,
+    autoHover: true,
+    slideMargin: 30.5
+  });
+});
